b5126edb01aa0835b5a13e2324f890535a7e7b2d
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-20 01:19:14 +0900

    Fix diagnosis route helper in controller test

--- test/controllers/diagnoses_controller_test.rb
+++ test/controllers/diagnoses_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class DiagnosesControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get diagnoses_top_url
+    get diagnosis_top_url
     assert_response :success
   end
 end
