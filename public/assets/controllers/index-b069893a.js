350777156185c7093b86a8eb0f0f0d8c41db1809
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-19 19:28:35 +0900

    Clean PR: remove cache files and apply header logo

--- public/assets/controllers/index-b069893a.js
+++
@@ -1,6 +0,0 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { Application } from "@hotwired/stimulus"
-import HelloController from "./hello_controller"
-
-window.Stimulus = Application.start()
-Stimulus.register("hello", HelloController)
--- public/assets/tailwindcss-2701f771.js
+++
@@ -1,4 +0,0 @@
-// tailwindcss@4.1.17 downloaded from https://ga.jspm.io/npm:tailwindcss@4.1.17/dist/lib.mjs
-
-export{D as Features,T as Polyfills,J as __unstable__loadDesignSystem,H as compile,B as compileAst,Y as default}from"../_/CkGx2Ba0.js";import"../_/CSLAA2v3.js";import"../_/Cay67bh1.js";
-
