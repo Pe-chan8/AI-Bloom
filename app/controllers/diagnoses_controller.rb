e8ff8e52139247be35ea57c0d2dad6b318b4051d
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-20 17:54:43 +0900

    修正

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,4 +1,13 @@
 class DiagnosesController < ApplicationController
   def top
   end
+
+  def questions
+    @questions = DiagnosisQuestion.order(:position)
+  end
+
+  def result
+    # ロジック未実装/ひとまず受け取った回答を読むだけ
+    @answers = params[:answers] || {}
+  end
 end
