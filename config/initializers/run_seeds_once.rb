17764b6c07477532cde331335cab6553b4f7b401
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-27 05:22:44 +0900

    Run seeds automatically once in production

--- config/initializers/run_seeds_once.rb
+++ config/initializers/run_seeds_once.rb
@@ -1,10 +1,11 @@
+# Run seeds once in production
 if Rails.env.production?
   seed_flag = Rails.root.join("tmp", "seed_done.txt")
 
   unless File.exist?(seed_flag)
     Rails.logger.info "Running seeds in production..."
     begin
-      require Rails.root.join("db/seeds.rb")
+      load Rails.root.join("db/seeds.rb")
       File.write(seed_flag, Time.now.to_s)
       Rails.logger.info "Seeds executed successfully."
     rescue => e
