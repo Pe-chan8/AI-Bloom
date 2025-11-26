ac88cd81e7181252b1a0e5e18bbcb6f2c4974015
Pe-chan8 <hr.tp.892837@gmail.com>
2025-11-27 04:39:28 +0900

    Fix current_buddy to use user.buddy instead of profile

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -7,18 +7,14 @@ class BuddiesController < ApplicationController
 
   def select
     buddy = Buddy.find(params[:id])
-
-    profile = current_user.profile || current_user.build_profile
-    profile.buddy = buddy
-    profile.save!
-
+    current_user.update!(buddy: buddy)
     redirect_to root_path, notice: "#{buddy.name} をバディに設定しました。"
   end
 
   private
 
   def current_buddy
-    current_user.profile&.buddy
+    current_user.buddy
   end
   helper_method :current_buddy
 end
