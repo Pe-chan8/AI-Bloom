59a367f815405536a400df67c6686968b8049e1a
Pe-chan8 <hr.tp.892837@gmail.com>
2025-12-01 18:37:10 +0900

    change-buddy

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,20 +1,21 @@
 class BuddiesController < ApplicationController
   before_action :authenticate_user!
 
+  before_action :set_buddy, only: :select
+
   def index
     @buddies = Buddy.where(is_active: true)
   end
 
+  # バディ変更
   def select
-    buddy = Buddy.find(params[:id])
-    current_user.update!(buddy: buddy)
-    redirect_to buddies_path, notice: "#{buddy.name} をバディに設定しました。"
+    current_user.update!(buddy: @buddy)
+    redirect_to buddies_path, notice: "#{@buddy.name} にバディを変更しました。"
   end
 
   private
 
-  def current_buddy
-    current_user.buddy
+  def set_buddy
+    @buddy = Buddy.find(params[:id])
   end
-  helper_method :current_buddy
 end
