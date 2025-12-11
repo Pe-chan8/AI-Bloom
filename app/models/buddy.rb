ee4fc20a5e66f37ad5f9bbfbeaabb3d0a13251bd
Pe-chan8 <hr.tp.892837@gmail.com>
2025-12-11 17:37:04 +0900

    render

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -5,4 +5,19 @@ class Buddy < ApplicationRecord
   validates :name, presence: true
 
   scope :active, -> { where(is_active: true) }
+
+  # code → ニックネーム の対応表
+  BUDDY_CODE_TO_NICKNAME = {
+    "amiable"       => "ルナ",
+    "analytical"    => "キエル",
+    "expressive"    => "エルフィ",
+    "driving"       => "ヴァル",
+    "normal"        => "ニル",
+    "kansai_friend" => "レオ"
+  }.freeze
+
+  # 画面で使う表示名
+  def display_name
+    BUDDY_CODE_TO_NICKNAME[code] || name
+  end
 end
