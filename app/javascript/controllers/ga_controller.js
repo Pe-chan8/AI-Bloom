e0d90ac60370b7ad88629502eecc889b81bd7be1
Pe-chan8 <hr.tp.892837@gmail.com>
2026-01-26 18:29:01 +0900

    add-ga4-controllers

--- app/javascript/controllers/ga_controller.js
+++ app/javascript/controllers/ga_controller.js
@@ -1,6 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    // Turbo Stream invoke が呼べるように DOM要素にメソッドを生やす
+    this.element.post_created = this.post_created.bind(this)
+  }
+
+  disconnect() {
+    delete this.element.post_created
+  }
+
   post_created() {
     if (typeof gtag === "function") {
       gtag("event", "post_created")
