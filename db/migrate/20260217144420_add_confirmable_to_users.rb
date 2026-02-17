70b00ff78f6d0b797bef65ab1aff2f7b9ce4a44c
Pe-chan8 <hr.tp.892837@gmail.com>
2026-02-18 03:04:22 +0900

    fix: correct migration class name for confirmable

--- db/migrate/20260217144420_add_confirmable_to_users.rb
+++ db/migrate/20260217144420_add_confirmable_to_users.rb
@@ -1,4 +1,4 @@
-class AddDeviseToUsers < ActiveRecord::Migration[8.1]
+class AddConfirmableToUsers < ActiveRecord::Migration[8.1]
   def change
     # no-op: users table already has devise columns
   end
