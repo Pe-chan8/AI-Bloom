6bae16e8ee20cb1849fb45a040179d395a632039
Pe-chan8 <hr.tp.892837@gmail.com>
2026-02-18 02:17:47 +0900

    feat:メールアドレス変更機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable,
-         :omniauthable, omniauth_providers: [:google_oauth2]
+         :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   # -------------------------------------------------------
   # OmniAuth からユーザーを作成/取得
--- lib/tasks/devise_mail.rake
+++ lib/tasks/devise_mail.rake
@@ -1,6 +1,6 @@
 namespace :devise do
   desc "Send confirmation instructions (development helper)"
-  task :send_confirmation, [:user_id] => :environment do |_, args|
+  task :send_confirmation, [ :user_id ] => :environment do |_, args|
     Rails.application.reload_routes!
     user = User.find(args[:user_id])
     user.send_confirmation_instructions
