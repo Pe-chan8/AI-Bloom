dd0edbac513693c601f20e28ee014c248fdb6344
Pe-chan8 <hr.tp.892837@gmail.com>
2026-02-18 18:35:48 +0900

    feat：AI-Bloomを育てる声（Googleフォーム導線）を追加

--- app/controllers/contacts_controller.rb
+++
@@ -1,7 +0,0 @@
-class ContactsController < ApplicationController
-  def new; end
-
-  def create
-    redirect_to new_contact_path, notice: "お問い合わせありがとうございます（準備中）"
-  end
-end
---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,8 @@
+class FeedbackController < ApplicationController
+
+  def new
+  end
+
+  def thanks
+  end
+end
